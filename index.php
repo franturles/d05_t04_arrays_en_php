5e3d454fcb95074efb1d5105ffbe2dc53f8af3d1
franturles <kikofran35@hotmail.com>
2022-01-14 11:46:12 +0000

    Array indexado

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Definicion de arrays en PHP</h3>
+        <?php
+        //Array indexado
+        $amigos=array("Anton","Rosalia","Xoan");
+        echo "O terceiro amigo e: " . $amigos[2]."<br/>";
+        ?>
+    </body>
+</html>

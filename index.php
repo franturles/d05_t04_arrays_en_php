f139a7ab44d49d703df6d5426f1262a7924454ed
franturles <kikofran35@hotmail.com>
2022-01-14 11:50:06 +0000

    Array asociativo

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
         //Array indexado
         $amigos=array("Anton","Rosalia","Xoan");
         echo "O terceiro amigo e: " . $amigos[2]."<br/>";
+
+        //Array asociativo
+        $dnisAmigos=array("Anton"=>"12345678x","Rosalia"=>"11112222X","Xoan"=>"99997777X");
+        echo "O DNI de Rosalia e:" . $dnisAmigos["Rosalia"]."<br/>";
         ?>
     </body>
 </html>
